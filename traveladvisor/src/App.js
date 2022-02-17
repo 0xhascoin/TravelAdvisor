27a40d966aec57e7ba9f2715dd5edc79c43b1750
hasanelmi678 <70804507+hasanelmi678@users.noreply.github.com>
2022-02-17 16:31:19 +0000

    Initial commit

---
+++ traveladvisor/src/App.js
@@ -0,0 +1,84 @@
+import React, { useEffect, useState } from 'react';
+import { CssBaseline, Grid } from '@material-ui/core';
+import { getPlacesData, getWeatherData } from './api'
+
+// Components
+import Header from './components/Header/Header';
+import List from './components/List/List';
+import Map from './components/Map/Map';
+
+function App() {
+  const [weatherData, setWeatherData] = useState([]);
+  const [places, setPlaces] = useState([]);
+  const [filteredPlaces, setFilteredPlaces] = useState([]);
+  const [coordinates, setCoordinates] = useState({});
+  const [bounds, setBounds] = useState({});
+  const [childClicked, setChildClicked] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [type, setType] = useState('restaurants');
+  const [rating, setRating] = useState('');
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(({ coords: { latitude, longitude } }) => {
+      setCoordinates({ lat: latitude, lng: longitude })
+    });
+  }, []);
+
+  useEffect(() => {
+    setFilteredPlaces(places.filter((place) => place.rating > rating));
+  }, [rating]);
+
+  useEffect(() => {
+    if (bounds.sw && bounds.ne) {
+      setIsLoading(true);
+
+      getWeatherData(coordinates.lat, coordinates.lng)
+      .then((data) => setWeatherData(data));
+
+      // console.log(weatherData, "Weather data")
+      
+      getPlacesData(type, bounds.sw, bounds.ne)
+      .then((data) => {
+        setPlaces(data?.filter((place) => place.name && place.num_reviews > 0));
+        setFilteredPlaces([]);
+        // console.log(data, "DATA");
+        setIsLoading(false);
+      })
+    }
+  }, [type, bounds]);
+
+  return (
+    <>
+      <CssBaseline />
+      <Header 
+        setCoordinates={setCoordinates}
+        />
+      <Grid container spacing={3} style={{ width: "100%" }}>
+        <Grid item xs={12} md={4}>
+          <List 
+            places={filteredPlaces.length ? filteredPlaces : places}
+            childClicked={childClicked}
+            isLoading={isLoading}
+            type={type}
+            setType={setType}
+            rating={rating}
+            setRating={setRating}
+            
+            />
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <Map 
+            coordinates={coordinates} 
+            setCoordinates={setCoordinates} 
+            setBounds={setBounds}
+            places={filteredPlaces.length ? filteredPlaces : places}
+            setChildClicked={setChildClicked}
+            weatherData={weatherData}
+            />
+        </Grid>
+      </Grid>
+    </>
+  );
+}
+
+export default App;
---
+++ traveladvisor/src/components/Map/styles.js
@@ -0,0 +1,20 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles(() => ({
+  paper: {
+    padding: '10px', display: 'flex', flexDirection: 'column', justifyContent: 'center', width: '100px',
+  },
+  mapContainer: {
+    height: '85vh', 
+    width: '100%',
+    marginTop: "10px",
+    borderRadius: '6px',
+    boxShadow: "box-shadow: 0 8px 30px rgb(0 29 54 / 10%)"
+  },
+  markerContainer: {
+    position: 'absolute', transform: 'translate(-50%, -50%)', zIndex: 1, '&:hover': { zIndex: 2 },
+  },
+  pointer: {
+    cursor: 'pointer',
+  },
+}));
